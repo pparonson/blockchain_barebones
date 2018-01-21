f2abbeabf0c0fb18f1f1e7dd9daefd9830262438
pparonson <preston.aronson@gmail.com>
2018-01-21 08:06:50 -0700

    complete add basic web app project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// import * as R from 'ramda';
+
+import * as math from './sum';
+
+console.log(`Answer: ${math.add(2, 1)}`);
+
+/*
+* index: index of the block in the chain (ie array)
+* timestamp: when the block was created
+* data: details of the transaction
+* previousHash: hash of previous block
+*/
