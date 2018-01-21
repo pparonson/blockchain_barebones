e1ae56be7084b6c91a18de315eb2d31ff19bced6
pparonson <preston.aronson@gmail.com>
2018-01-21 08:09:51 -0700

    remove feature from basic project

--- src/index.js
+++ src/index.js
@@ -3,10 +3,3 @@
 import * as math from './sum';
 
 console.log(`Answer: ${math.add(2, 1)}`);
-
-/*
-* index: index of the block in the chain (ie array)
-* timestamp: when the block was created
-* data: details of the transaction
-* previousHash: hash of previous block
-*/
